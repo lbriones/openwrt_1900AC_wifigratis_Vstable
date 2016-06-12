3fed463600c29a0d0e47eccb13f3044544ee9a61
Luis Briones <lbriones@users.noreply.github.com>
2016-06-12 19:45:48 -0300

    add hostname in data extract

--- root/sendData.sh
+++ root/sendData.sh
@@ -9,14 +9,13 @@ do
   do
     RSSI=`iw dev $interface station get $mac|grep signal:|cut -f 3 -s -d" "`
     RSSI="${RSSI//[!0-9]/}"
-    SSID_NAME=`iwinfo|grep ESSID|sed 's|ESSID\|\"||g'|grep -w "$interface."|cut -f 2 -s -d":"|sed 's/^ *//g' | sed 's/ *$//g'`
+    SSID_NAME=`iw "$interface." info|grep ESSID|sed 's|ESSID\|\"||g'|grep -w "$interface."|cut -f 2 -s -d":"|sed 's/^ *//g' | sed 's/ *$//g'`
     SSID_KEY=`echo -n $ROUTER_MAC$SSID_NAME | md5sum| awk '{ print $1 }'|sed 's/^ *//g' | sed 's/ *$//g'`
     SSID_MAC=`ifconfig|grep  -w "$interface."|awk '/HWaddr/ {print $5}'`
-    host=""
-    host=`cat /tmp/dhcp.leases | cut -f 2,3,4 -s -d" " | grep $mac | cut -f 3 -s -d" "`
+    HOST=`cat /tmp/dhcp.leases | cut -f 2,3,4 -s -d" " | grep $mac | cut -f 3 -s -d" "`
         #echo "$mac=$RSSI=$SSID_KEY=$SSID_MAC=$SSID_NAME=$ROUTER_KEY=$ROUTER_MAC"
-        echo "mac=$mac&rssi=$RSSI&SSID_KEY=$SSID_KEY&SSID_MAC=$SSID_MAC&SSID_NAME=$SSID_NAME&ROUTER_KEY=$ROUTER_KEY&ROUTER_MAC=$ROUTER_MAC"
-        QUERY="http://neursoft.com/routerData/addDataSSID.php?mac=$mac&rssi=$RSSI&SSID_KEY=$SSID_KEY&SSID_MAC=$SSID_MAC&SSID_NAME=$SSID_NAME&ROUTER_KEY=$ROUTER_KEY&ROUTER_MAC=$ROUTER_MAC"
+        echo "HOST=$HOST&mac=$mac&rssi=$RSSI&SSID_KEY=$SSID_KEY&SSID_MAC=$SSID_MAC&SSID_NAME=$SSID_NAME&ROUTER_KEY=$ROUTER_KEY&ROUTER_MAC=$ROUTER_MAC"
+        QUERY="http://neursoft.com/captive/addDataSSID.php?HOST=$HOST&mac=$mac&rssi=$RSSI&SSID_KEY=$SSID_KEY&SSID_MAC=$SSID_MAC&SSID_NAME=$SSID_NAME&ROUTER_KEY=$ROUTER_KEY&ROUTER_MAC=$ROUTER_MAC"
         QUERY=`echo $QUERY|sed 's/ /%20/g'`
         #echo $QUERY
         CODE=`wget -s $QUERY`
