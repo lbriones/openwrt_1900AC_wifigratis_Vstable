ecc9c5357501e0b6d9562f9be0e61cdb820578ff
Luis Briones <lbriones@users.noreply.github.com>
2016-09-30 02:02:00 -0300

    Create get_mac_around.sh

---
+++ root/get_mac_around.sh
@@ -0,0 +1,10 @@
+iw dev wlan0 scan | awk '/^BSS/ {
+    mac = gensub ( /^BSS[[:space:]]*([0-9a-fA-F:]+).*?$/, "\\1", "g", $0 );
+}
+/^[[:space:]]*signal:/ {
+    signal = gensub ( /^[[:space:]]*signal:[[:space:]]*(\-?[0-9.]+).*?$/, "\\1", "g", $0 );
+}
+/^[[:space:]]*SSID:/ {
+    ssid = gensub ( /^[[:space:]]*SSID:[[:space:]]*([^\n]*).*?$/, "\\1", "g", $0 );
+    printf ( "%s %s %s\n", signal, mac, ssid );
+}'
